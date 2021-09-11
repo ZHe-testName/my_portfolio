8144b35962cc2cb544a0ef143ac8f728581f8d6c
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-09-11 18:06:18 +0300

    ***

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,7 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-      <HashRouter>
         <App />
-      </HashRouter>
     </React.StrictMode>,
   document.getElementById('root')
 );
