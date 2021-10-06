cae589c9eb0f62d7d1a876e0309498264ce6b7bc
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-10-06 21:15:59 +0300

    Almost finished with home page styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import c from './app.module.css';
 import Home from './components/home/Home';
 import Contacts from './components/contacts/Contacts';
 import MyWorks from './components/my_works/MyWorks';
-import Skills from './components/skills/Skills';
 import AboutMe from './components/about_me/AboutMe';
 import NavBar from './components/nav_bar/NavBar';
 import RemoteJob from './components/remote_job/RemoteJob';
@@ -16,7 +15,7 @@ function App() {
     <main className={c.app}>
         <NavBar />
 
-        <div className="mainContent">
+        <div className={c.mainContent}>
           <Route path={'/'} exact render={() => <Redirect to="/home"/>}/>
 
           <Route path="/home" render={() => <Home/>}/>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,16 +4,25 @@ import c from './home.module.css';
 
 function Home() {
     return (
-        <section className={c.aboutMe}>
-            <div className={c.aboutMeWrap}>
-                <div className={c.aboutMeDescription}>
-                    <p>Привет!</p>
-                    <p>Меня зовут Хорунжий Евгений</p>
-                    <p>Я Front-End разработчик</p>
+        <section className={c.myHome}>
+            <div className={c.homeMeWrap}>
+                <div className={c.homeMeImage}>
                 </div>
 
-                <div className={c.aboutMeImage}>
-                    Я бла-бла-бла...
+                <div className={c.homeMeDescription}>
+                    <h1>
+                        Я ХОРУНЖИЙ ЕВГЕНИЙ
+                    </h1>
+
+                    <span>JS РАЗРАБОТЧИК</span>
+
+                    <p>
+                        Я веб-разработчик из Украины, специализирующийся на создании чистого и удобного интерфейса и приложений. 
+                    </p>
+                    <p>
+                        Увлечен созданием отличного программного обеспечения, которое улучшит жизнь окружающих меня людей.
+                    </p>
+                    
                 </div>
             </div>
         </section>
