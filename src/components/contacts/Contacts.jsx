c58e55528bf1f8b8a1a8bf3be626a10e3676e518
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-11-11 17:34:15 +0200

    Fixed form css rules for validation

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -83,6 +83,34 @@ function Contacts({ onSubmit }) {
     );
 };
 
+const validate = values => {
+    const errors = {};
+
+    if (!values.name){
+        errors.name = 'Name is Required';
+    }else if (values.name.length >= 15){
+        errors.name = 'Must be 15 characters or less';
+    }
+
+    if (!values.email){
+        errors.email = 'Email is Required';
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email = 'Incorect e-mail adress';
+    }
+
+    if (!values.subject){
+        errors.subject = 'Subject is Required';
+    }else if (values.name.length >= 25){
+        errors.name = 'Must be 25 characters or less';
+    }
+
+    if (!values.message){
+        errors.message = 'Message is Required';
+    }
+
+    return errors;
+};
+
 const SendMailForm = ({onSubmit}) => {
     const formik = useFormik({
         initialValues: {
@@ -91,6 +119,8 @@ const SendMailForm = ({onSubmit}) => {
             subject: '',
             message: '',
         },
+
+        validate,
     
         onSubmit: (values, e) => {
             onSubmit(values);
@@ -102,34 +132,58 @@ const SendMailForm = ({onSubmit}) => {
     return (
         <form onSubmit={formik.handleSubmit}>
             <div className={c.inputWrap}>
-                <input 
-                    name="name"
-                    type="text" 
-                    placeholder="YOUR NAME"
-                    onChange={formik.handleChange}
-                    value={formik.values.name}/>
+                <div>
+                    <input 
+                        name="name"
+                        type="text" 
+                        placeholder="YOUR NAME"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}/>
 
-                <input 
-                    name="email"
-                    type="text" 
-                    placeholder="YOUR EMAIL"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}/>
+                        {formik.errors.name && formik.touched.name 
+                            ? <div>{formik.errors.name}</div> 
+                            : null}
+                </div>
 
-                <input 
-                    name="subject"
-                    type="text" 
-                    placeholder="YOUR SUBJECT"
-                    onChange={formik.handleChange}
-                    value={formik.values.subject}/>
+                <div>
+                    <input 
+                        name="email"
+                        type="text" 
+                        placeholder="YOUR EMAIL"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}/>
+
+                        {formik.errors.email && formik.touched.email 
+                            ? <div>{formik.errors.email}</div> 
+                            : null}
+                </div>
+
+                <div>
+                    <input 
+                        name="subject"
+                        type="text" 
+                        placeholder="YOUR SUBJECT"
+                        onChange={formik.handleChange}
+                        value={formik.values.subject}/>
+
+                        {formik.errors.subject && formik.touched.subject 
+                            ? <div>{formik.errors.subject}</div> 
+                            : null}
+                </div>
             </div>
 
-            <textarea 
-                name="message" 
-                maxLength='210' 
-                placeholder="YOUR MESSAGE"
-                onChange={formik.handleChange}
-                value={formik.values.message}></textarea>
+            <div className={c.textareaWrap}>
+                <textarea 
+                    name="message" 
+                    maxLength='210' 
+                    placeholder="YOUR MESSAGE"
+                    onChange={formik.handleChange}
+                    value={formik.values.message}></textarea>
+
+                    {formik.errors.message && formik.touched.message 
+                        ? <div>{formik.errors.message}</div> 
+                        : null}
+            </div>
 
             <div className={c.buttonWrap}>
                 <Button title="Отправить" type="submit"/>
