58699fe96cc1c64c55978318627d4fc3d2b8737a
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-01-09 17:12:26 +0200

    Updates

---
+++ precache-manifest.c4b0a6dbf7bdf539a0b7d2187f599fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35172621a52b10a375baaafe095c6e89",
+    "url": "/TS_ToDoList/index.html"
+  },
+  {
+    "revision": "2625b8a363570e93cb2b",
+    "url": "/TS_ToDoList/static/css/main.f9334bd0.chunk.css"
+  },
+  {
+    "revision": "b12e0e39dcb2faff3816",
+    "url": "/TS_ToDoList/static/js/2.8aed03be.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TS_ToDoList/static/js/2.8aed03be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2625b8a363570e93cb2b",
+    "url": "/TS_ToDoList/static/js/main.779379fa.chunk.js"
+  },
+  {
+    "revision": "402ceee8030a93b132cb",
+    "url": "/TS_ToDoList/static/js/runtime-main.6853f97d.js"
+  }
+]);
